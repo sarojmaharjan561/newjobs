a789a99f6958648076fefeb6ee071f295859b67a
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2025-01-01 09:47:08 +1100

    added layout, card component, tag component and configure tailwind and vite

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,21 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">
+        Companies Name
+    </div>
+
+    <div class="py-8 font-bold">
+        <h3>Job Description</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+
+        </div>
+        <img src="https://placehold.co/42" alt="" class="rounded-xl">
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
     ],
     theme: {
         extend: {
+            colors: {
+                black: "#060606",
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
