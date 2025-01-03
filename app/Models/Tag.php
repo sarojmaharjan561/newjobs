cf532fc063d410d4ea2417686a723c8807d71965
Saroj Maharjan <saroj.maharjan.561@gmail.com>
2025-01-03 21:01:00 +1100

    added search function

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -11,4 +12,9 @@ class Tag extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function jobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\JobController;
-use App\Http\Controllers\RegisteredUserController;
-use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\TagController;
 
 Route::get('/', [JobController::class, 'index']);
 
@@ -15,3 +17,7 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::delete('/logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+Route::get('/search', SearchController::class);
+
+Route::get('/tags/{tag:name}', TagController::class);
